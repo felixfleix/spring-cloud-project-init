4e7f98788a1fb248408403b0f7210d99c6ac7b7c
felix <xpf14561456@gmail.com>
2025-11-03 16:15:58 +0800

    feat(dataOp-support): initializing mybatis/mybatis-plus support

--- dataOp-support/mybatis-support/src/main/java/com/felix/configuration/MybatisConfiguration.java
+++ dataOp-support/mybatis-support/src/main/java/com/felix/configuration/MybatisConfiguration.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan(basePackages = {"com.felix.mapper"})
+@MapperScan(basePackages = {"com.felix.infrastructure.mapper"})
 public class MybatisConfiguration {
 
 
